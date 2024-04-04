7e297506a3a59022b045cb355382ab857250a990
martipinell10 <martipime@outlook.es>
2024-04-04 23:02:44 +0200

    route53.tf

--- compute.tf
+++ compute.tf
@@ -57,40 +57,40 @@ resource "aws_autoscaling_group" "jumphost_asg" {
 # -----------------------------------------------
 
 #resource "aws_lb" "wordpress_alb" {
-#  name               = "wordpress-alb"
-#  internal           = false
-#  load_balancer_type = "application"
-#  ip_address_type    = "ipv4"
-#  security_groups    = [aws_security_group.alb_sg.id]
-#  subnets            = aws_subnet.public_subnets.*.id
-#}
+  name               = "wordpress-alb"
+  internal           = false
+  load_balancer_type = "application"
+  ip_address_type    = "ipv4"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = aws_subnet.public_subnets.*.id
+}
 
 #resource "aws_lb_target_group" "alb_target_grp" {
-#  name        = "wordpress-alb-tg"
-#  port        = 80
-#  protocol    = "HTTP"
-#  target_type = "instance"
-#  vpc_id      = aws_vpc.project_vpc.id
-#  health_check {
-#    enabled             = true
-#    healthy_threshold   = 3
-#    interval            = 15
-#    matcher             = 200
-#    path                = "/"
-#    timeout             = 3
-#    unhealthy_threshold = 3
-#  }
-#}
+  name        = "wordpress-alb-tg"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "instance"
+  vpc_id      = aws_vpc.project_vpc.id
+  health_check {
+    enabled             = true
+    healthy_threshold   = 3
+    interval            = 15
+    matcher             = 200
+    path                = "/"
+    timeout             = 3
+    unhealthy_threshold = 3
+  }
+}
 
 #resource "aws_lb_listener" "alb_listener" {
-#  load_balancer_arn = aws_lb.wordpress_alb.arn
-#  port              = 80
-#  protocol          = "HTTP"
-#  default_action {
-#    type             = "forward"
-#    target_group_arn = aws_lb_target_group.alb_target_grp.arn
-#  }
-#}
+  load_balancer_arn = aws_lb.wordpress_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alb_target_grp.arn
+  }
+}
 
 # -------------------------------------------------
 
