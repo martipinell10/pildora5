4da63769077f9433d8b7e6277d7ee28f19427de8
martipinell10 <martipime@outlook.es>
2024-04-18 20:18:16 +0200

    Update backend.tf

--- marti/exemple1/backend.tf
+++ marti/exemple1/backend.tf
@@ -1,7 +1,7 @@
 # Emmagatzemant per l'estat dels fitxers dintre del s3 bucket.
 terraform {
   backend "s3" {
-    bucket                  = "wordpress-pildora5-01-01"
+    bucket                  = "wordpress-pildora5"
     region                  = "us-east-1"
     key                     = "wordpress-3tier/terraform.tfstate"
     shared_credentials_file = "~/.aws/credentials"
